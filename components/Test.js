dc5ac8cca7fb744fe52fe471d0da9efc600af9d4
HIKALU <play3a@126.com>
2015-11-29 23:20:38 +0800

    make app js

---
+++ components/Test.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Test = React.createClass({
+render: function() {
+  return (
+
+    <div className="hikalu clearfix">
+      <h2>zgl</h2>
+      <p>22333333</p>
+    </div>
+  );
+}
+});
+module.exports = Test;
